cf8405d95aea38f0af56340b9f7a74a8222dd167
bobobo00 <48381156+bobobo00@users.noreply.github.com>
2019-11-15 22:34:08 +0800

    模拟发送方-实现多次发送

--- UDPClient.java
+++ UDPClient.java
@@ -1,5 +1,7 @@
 package UDP;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
@@ -20,12 +22,32 @@ public class UDPClient {
 		//使用DatagramSocket 指定端口，创建发送端。
 		DatagramSocket client=new DatagramSocket(8888);
 		//准备数据，封装成DatagramPacket包裹，需指定目的地
-		String data="hello 明天";
-		byte[] datas=data.getBytes();
-		DatagramPacket packet=new DatagramPacket(datas,0,datas.length,new InetSocketAddress("localhost",9999));
-		//发送包裹send(DatagramPacket)
-		client.send(packet);
-		//释放资源
+		/*ByteArrayOutputStream bos=new ByteArrayOutputStream();
+		DataOutputStream ts=new DataOutputStream(new BufferedOutputStream(bos));
+		ts.write(1);
+		ts.writeBytes("aaaa");
+		ts.writeBoolean(false);
+		ts.flush();*/
+		/*File file=new File("E:\\workspace-java\\多线程\\g.jpg");
+		InputStream is=new BufferedInputStream(new FileInputStream(file));
+		ByteArrayOutputStream bos=new ByteArrayOutputStream();
+		byte[] flush=new byte[1024];
+		while(is.read(flush)!=-1) {
+			bos.write(flush);
+		}
+		bos.flush();*/
+		BufferedReader bar=new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			String bos=bar.readLine();
+			byte[] datas=bos.getBytes();
+			DatagramPacket packet=new DatagramPacket(datas,0,datas.length,new InetSocketAddress("localhost",6666));
+			//发送包裹send(DatagramPacket)
+			client.send(packet);
+			if(bos.equals("bye")) {
+				break;
+			}
+		}
+		System.out.println("发送结束");
 		client.close();
 	}
 
